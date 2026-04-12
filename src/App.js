f21858aedaa7ccb22c0478094d1a766cf73a9fe1
faisalwilliam <williamfaisal423@gmail.com>
2026-04-12 12:03:41 +0200

    version2

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@
 function App() {
   return (
     <>
+
       <h1 className="title">Version 1 React App</h1>
-    <h2>Welcome to React</h2>
+    <h2 className="subtitle">Welcome to React</h2>
+    <h3 className="subtitle">This is a simple React application.</h3>
     </>
     
     
