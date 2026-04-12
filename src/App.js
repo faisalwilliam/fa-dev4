022ba4c384fb01b05a0bdae62a2528000ae507c0
faisalwilliam <williamfaisal423@gmail.com>
2026-04-12 15:07:59 +0200

    branch1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
 function App() {
   return (
     <>
-
+         <h4>new branch</h4>
       <h1 className="title">Version 1 React App</h1>
     <h2 className="subtitle">Welcome to React</h2>
     <h3 className="subtitle">This is a simple React application.</h3>
